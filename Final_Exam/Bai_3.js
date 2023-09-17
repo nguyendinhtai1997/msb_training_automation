7dff770a59a0b3d94b73be5d61b46e3095ce8ebf
Hanglt496 <Hangvicc1996@gmail.com>
2023-09-17 08:31:08 +0700

    clean code Bai_3

--- Final_Exam/Bai_3.js
+++ Final_Exam/Bai_3.js
@@ -3,26 +3,23 @@ pm.test("Status code is 200", function () {
     pm.response.to.have.status(200);
 });
 
-//Check name = "Automation_MSB_Project_API"
+//Get response
 var jsonData = pm.response.json();
+
+//Check name = "Automation_MSB_Project_API"
 var name_content = jsonData.name
 pm.test("Check name", function () {
-    var jsonData = pm.response.json();
     pm.expect(name_content).to.eql("Automation_MSB_Project_API");
 });
 
 //Check color = "charcoal"
-var jsonData = pm.response.json();
 var color_content = jsonData.color
 pm.test("Check color", function () {
-    var jsonData = pm.response.json();
     pm.expect(color_content).to.eql("charcoal");
 });
 
 //Check view_style = "list"
-var jsonData = pm.response.json();
 var view_style_content = jsonData.view_style
 pm.test("Check view_style", function () {
-    var jsonData = pm.response.json();
     pm.expect(view_style_content).to.eql("list");
 });
